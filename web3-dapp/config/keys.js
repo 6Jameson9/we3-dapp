05e78c13f2f7be3b4af021ade3dd1528f24659ad
6Jameson9 <126174757+6Jameson9@users.noreply.github.com>
2024-02-28 14:22:50 +0600

    web3-dapp

--- web3-dapp/config/keys.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-const jwtSecret = crypto.randomBytes(32).toString('hex');
-
-module.exports = {
-  JWT_SECRET: jwtSecret,
-  MONGO_URI: 'mongodb://localhost:27017/myapp'
-};
